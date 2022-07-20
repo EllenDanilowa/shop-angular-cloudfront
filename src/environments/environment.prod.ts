d4c8c0177a2f5dd7d7959c82b6e6a6d6ad84a221
Alena Bachyla <alena_bachyla@epam.com>
2022-07-21 00:59:55 +0200

    update api links

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ import { Config } from './config.interface';
 export const environment: Config = {
   production: true,
   apiEndpoints: {
-    product: 'https://myln05b7e4.execute-api.eu-west-1.amazonaws.com',
+    product: 'https://kun8h51167.execute-api.eu-west-1.amazonaws.com',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
